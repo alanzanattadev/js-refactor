425801eb9a4a857d1a5f3938d8119e96b3386dc4
alanzanattadev <alan.zanatta@outlook.com>
2016-12-31 10:12:01 +0100

    set flow parser as default config

--- __tests__/move-file-js.js
+++ __tests__/move-file-js.js
@@ -3,4 +3,4 @@ const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 defineTest(__dirname, 'move-file', {fromPath: "./__testfixtures__/SimpleIntraModuleFile.input.js", toPath: "./__tests__/SimpleIntraModuleFile.output.js", currentModuleName: "veyo-commons"}, 'SimpleIntraModuleFile');
 defineTest(__dirname, 'move-file', {fromPath: "./__testfixtures__/SimpleExtraModuleFile.input.js", toPath: "./__tests__/SimpleExtraModuleFile.output.js", moduleName: "veyo-commons"}, 'SimpleExtraModuleFile');
 defineTest(__dirname, 'move-file', {fromPath: "./presenters/caregiverLead/CaregiverAvailabilityForm.js", toPath: "./containers/caregiverLead/CaregiverAvailabilityForm.js", currentModuleName: "veyo-commons"}, 'ComplexIntraModuleFile');
-defineTest(__dirname, 'move-file', {fromPath: "./components/presenters/misc/ResultBox.js", toPath: "./components/atoms/ResultBox.js", moduleName: "veyo-commons"}, 'ComplexExtraModuleFile');
+defineTest(__dirname, 'move-file', {fromPath: "./components/presenters/misc/ResultBox.js", toPath: "./components/atoms/ResultBox.js", moduleName: "veyo-commons", parser: 'flow'}, 'ComplexExtraModuleFile');
